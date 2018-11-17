0f830c9a6c9719b67690967679dadb8281e4d01a
parksodam0328 <parksodam0228@e-mirim.hs.kr>
2018-11-17 22:43:05 +0900

    HomeFragment 배치 완료

--- app/src/main/java/kr/hs/emirim/parksodam/chokchoksandwich/Home/HomeFragment.java
+++ app/src/main/java/kr/hs/emirim/parksodam/chokchoksandwich/Home/HomeFragment.java
@@ -1,18 +1,36 @@
 package kr.hs.emirim.parksodam.chokchoksandwich.Home;
 
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.ArrayList;
 
 import kr.hs.emirim.parksodam.chokchoksandwich.BaseFragment;
 import kr.hs.emirim.parksodam.chokchoksandwich.R;
 
 public class HomeFragment extends BaseFragment {
-
+    private DatabaseReference mRecommendSandwich;
+    private DatabaseReference mEvent;
+    int i,j;
+    final public static String TAG = "촉촉샌드위치:Home";
+    ArrayList<ImageView> eventImage = null;
+    ArrayList<ImageView> sandwichImage = null;
+    ArrayList<TextView> sandwichText = null;
     public HomeFragment() {
         // Required empty public constructor
     }
@@ -25,6 +43,12 @@ public class HomeFragment extends BaseFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        mRecommendSandwich = FirebaseDatabase.getInstance().getReference("subway/recommend");
+        mEvent = FirebaseDatabase.getInstance().getReference("subway/event");
+        eventImage = new ArrayList<ImageView>();
+        sandwichImage = new ArrayList<ImageView>();
+        sandwichText = new ArrayList<TextView>();
+
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
         ImageView titleImage = (ImageView) view.findViewById(R.id.titleImage);
@@ -33,29 +57,66 @@ public class HomeFragment extends BaseFragment {
         ImageView recommend3 = (ImageView) view.findViewById(R.id.recommend3);
         ImageView recommend4 = (ImageView) view.findViewById(R.id.recommend4);
 
+        final ImageView event1 = (ImageView) view.findViewById(R.id.event1);
+        ImageView event2 = (ImageView) view.findViewById(R.id.event2);
+        ImageView event3 = (ImageView) view.findViewById(R.id.event3);
+
+        TextView recommendText1 = (TextView) view.findViewById(R.id.text1);
+        TextView recommendText2 = (TextView) view.findViewById(R.id.text2);
+        TextView recommendText3 = (TextView) view.findViewById(R.id.text3);
+        TextView recommendText4 = (TextView) view.findViewById(R.id.text4);
+
+        eventImage.add(event1); eventImage.add(event2); eventImage.add(event3);
+        sandwichImage.add(recommend1); sandwichImage.add(recommend2); sandwichImage.add(recommend3); sandwichImage.add(recommend4);
+        sandwichText.add(recommendText1); sandwichText.add(recommendText2); sandwichText.add(recommendText3); sandwichText.add(recommendText4);
+
+        mRecommendSandwich.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                i=0;
+                for (DataSnapshot recommend : dataSnapshot.getChildren()) { //모든 유저 정보 가져오기
+                    Log.e(TAG,recommend.getValue().toString());
+                    //추천샌드위치 이미지
+                    Glide.with(getActivity())
+                            .load(recommend.getValue().toString())
+                            .override(130,80)
+                            .into(sandwichImage.get(i));
+                    sandwichText.get(i).setText(recommend.getKey());
+                    i++;
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        mEvent.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                j=0;
+                for (DataSnapshot event : dataSnapshot.getChildren()) { //모든 유저 정보 가져오기
+                    //추천샌드위치 이미지
+                    Glide.with(getActivity())
+                            .load(event.getValue().toString())
+                            .override(130,80)
+                            .into(eventImage.get(j));
+                    j++;
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
 
         Glide.with(this) //타이틀 이미지
                 .load("https://firebasestorage.googleapis.com/v0/b/chokchoksandwich-246bf.appspot.com/o/logo%2Ftitle.png?alt=media&token=357cf5d2-ed57-47d5-af1b-d54358cc4a67")
-                .override(400,290)
+                .override(430,320)
                 .into(titleImage);
 
-        // 추천 샌드위치 이미지
-        Glide.with(this)
-                .load("https://firebasestorage.googleapis.com/v0/b/chokchoksandwich-246bf.appspot.com/o/subway%2Fsandwich%2Fblt.jpg?alt=media&token=3f51858d-9ac3-41fa-9d68-a192f58091fc")
-                .override(130,80)
-                .into(recommend1);
-        Glide.with(this)
-                .load("https://firebasestorage.googleapis.com/v0/b/chokchoksandwich-246bf.appspot.com/o/subway%2Fsandwich%2Fbmt.jpg?alt=media&token=d3824ae5-a3b3-4dc6-81ec-ea0e59578296")
-                .override(130,80)
-                .into(recommend2);
-        Glide.with(this)
-                .load("https://firebasestorage.googleapis.com/v0/b/chokchoksandwich-246bf.appspot.com/o/subway%2Fsandwich%2Fegg.jpg?alt=media&token=3b5b6009-c5de-4c14-b293-27e12c3088fd")
-                .override(130,80)
-                .into(recommend3);
-        Glide.with(this)
-                .load("https://firebasestorage.googleapis.com/v0/b/chokchoksandwich-246bf.appspot.com/o/subway%2Fsandwich%2Fham.jpg?alt=media&token=63fcce93-8a72-4398-8955-fbe2a01342cb")
-                .override(130,80)
-                .into(recommend4);
         return view;
     }
 
--- app/src/main/java/kr/hs/emirim/parksodam/chokchoksandwich/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/parksodam/chokchoksandwich/MainActivity.java
@@ -1,5 +1,6 @@
 package kr.hs.emirim.parksodam.chokchoksandwich;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -15,81 +16,96 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.firebase.auth.FirebaseAuth;
 
 import kr.hs.emirim.parksodam.chokchoksandwich.User.LoginActivity;
 import kr.hs.emirim.parksodam.chokchoksandwich.User.RegisterActivity;
 
-import static kr.hs.emirim.parksodam.chokchoksandwich.R.id.titleImage;
-
+import static kr.hs.emirim.parksodam.chokchoksandwich.R.id.logoImage;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String WIFI_STATE = "WIFE";
-    public static final String MOBILE_STATE = "MOBILE";
-    public static final String NONE_STATE = "NONE";
     final public static String TAG = "촉촉샌드위치:LoginAct";
+    ConnectivityManager connectivityManager = null;
+    NetworkInfo networkInfo = null;
+    ImageView logoImage = null;
+    ImageView titleImage = null;
+    ImageButton login_btn = null;
+    ImageButton register_btn = null;
+    AlertDialog.Builder connectionNetwork = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ConnectivityManager connectivityManager = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
-        AlertDialog.Builder connectionNetwork = new AlertDialog.Builder(new ContextThemeWrapper(this, R.style.AlertDialogCustom));
 
-        if(networkInfo != null && networkInfo.isConnected() == true) { //무선 데이터 네트워크 또는 Wifi연결이 되어있는 상태
-            Log.e("aa", "들어옴");
-            ImageView logoImage = (ImageView) findViewById(R.id.logoImage);
-            ImageView titleImage = (ImageView) findViewById(R.id.titleImage);
-            ImageButton login_btn = (ImageButton) findViewById(R.id.login_btn);
-            ImageButton register_btn = (ImageButton) findViewById(R.id.register_btn);
+        logoImage = (ImageView) findViewById(R.id.logoImage);
+        titleImage = (ImageView) findViewById(R.id.titleImage);
+        login_btn = (ImageButton) findViewById(R.id.login_btn);
+        register_btn = (ImageButton) findViewById(R.id.register_btn);
+        connectionNetwork = new AlertDialog.Builder(new ContextThemeWrapper(this, R.style.AlertDialogCustom));
+        connectivityManager = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
+        networkInfo = connectivityManager.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) { //무선 데이터 네트워크 또는 Wifi연결이 되어있는 상태
 
-            Glide.with(this) //로고 이미지
-                    .load("https://firebasestorage.googleapis.com/v0/b/chokchoksandwich-246bf.appspot.com/o/logo%2Flogo.jpg?alt=media&token=51133882-f4f8-469f-8774-480ef5f95103")
-                    .error(R.drawable.logo)
-                    .override(350, 450)
-                    .into(logoImage);
+                Log.e("aa", "들어옴");
+               showImages();
+        } else {
+            showImages();
+            networkInfo = connectivityManager.getActiveNetworkInfo();
+        }
+    }
 
-            Glide.with(this) //타이틀 이미지
-                    .load("https://firebasestorage.googleapis.com/v0/b/chokchoksandwich-246bf.appspot.com/o/logo%2Ftitle.png?alt=media&token=357cf5d2-ed57-47d5-af1b-d54358cc4a67")
-                    .error(R.drawable.title)
-                    .override(360, 250)
-                    .into(titleImage);
+    public void editSetting() {
+        Log.e(TAG,"NETWORK5");
+        Log.e("aa", "editSetting function");
+        Intent intent = new Intent(Settings.ACTION_WIFI_SETTINGS);
+        startActivity(intent);
+    }
 
-            Glide.with(this) //로그인 버튼 이미지
-                    .load("https://firebasestorage.googleapis.com/v0/b/chokchoksandwich-246bf.appspot.com/o/images%2Flogin_btn.png?alt=media&token=6654ab6a-ba8b-4627-b1d2-842f55359186")
-                    .error(R.drawable.login_btn)
-                    .override(650, 150)
-                    .into(login_btn);
+    private void showImages() {
+        if (networkInfo != null && networkInfo.isConnected()) {
+                Glide.with(this) //로고 이미지
+                        .load("https://firebasestorage.googleapis.com/v0/b/chokchoksandwich-246bf.appspot.com/o/logo%2Flogo.jpg?alt=media&token=51133882-f4f8-469f-8774-480ef5f95103")
+                        .error(R.drawable.logo)
+                        .override(350, 450)
+                        .into(logoImage);
 
-            Glide.with(this) //회원가입 버튼 이미지
-                    .load("https://firebasestorage.googleapis.com/v0/b/chokchoksandwich-246bf.appspot.com/o/images%2Fregister_btn.png?alt=media&token=47b0c7fc-d5c1-4125-86e3-707489aca086")
-                    .error(R.drawable.register_btn)
-                    .override(650, 150)
-                    .into(register_btn);
+                Glide.with(this) //타이틀 이미지
+                        .load("https://firebasestorage.googleapis.com/v0/b/chokchoksandwich-246bf.appspot.com/o/logo%2Ftitle.png?alt=media&token=357cf5d2-ed57-47d5-af1b-d54358cc4a67")
+                        .error(R.drawable.title)
+                        .override(360, 250)
+                        .into(titleImage);
 
-            //로그인 이미지 클릭시 로그인 화면으로 이동
-            login_btn.setOnClickListener(new ImageButton.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent login_intent = new Intent(getApplicationContext(), LoginActivity.class);
-                    startActivity(login_intent);
-                }
-            });
+                Glide.with(this) //로그인 버튼 이미지
+                        .load("https://firebasestorage.googleapis.com/v0/b/chokchoksandwich-246bf.appspot.com/o/images%2Flogin_btn.png?alt=media&token=6654ab6a-ba8b-4627-b1d2-842f55359186")
+                        .error(R.drawable.login_btn)
+                        .override(650, 150)
+                        .into(login_btn);
 
-            //회원가입 이미지 클릭시 회원가입 화면으로 이동
-            register_btn.setOnClickListener(new ImageButton.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent register_intent = new Intent(getApplicationContext(), RegisterActivity.class);
-                    startActivity(register_intent);
-                }
-            });
+                Glide.with(this) //회원가입 버튼 이미지
+                        .load("https://firebasestorage.googleapis.com/v0/b/chokchoksandwich-246bf.appspot.com/o/images%2Fregister_btn.png?alt=media&token=47b0c7fc-d5c1-4125-86e3-707489aca086")
+                        .error(R.drawable.register_btn)
+                        .override(650, 150)
+                        .into(register_btn);
+
+                //로그인 이미지 클릭시 로그인 화면으로 이동
+                login_btn.setOnClickListener(new ImageButton.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent login_intent = new Intent(getApplicationContext(), LoginActivity.class);
+                        startActivity(login_intent);
+                    }
+                });
+
+                //회원가입 이미지 클릭시 회원가입 화면으로 이동
+                register_btn.setOnClickListener(new ImageButton.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent register_intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                        startActivity(register_intent);
+                    }
+                });
         } else {
             connectionNetwork.setTitle("네트워크 연결");
             connectionNetwork
@@ -100,6 +116,7 @@ public class MainActivity extends AppCompatActivity {
                                 @Override
                                 public void onClick(DialogInterface dialog, int which) {
                                     editSetting();
+                                    return;
                                 }
                             })
                     .setNegativeButton("취소", new DialogInterface.OnClickListener() {
@@ -109,13 +126,7 @@ public class MainActivity extends AppCompatActivity {
                         }
                     });
             connectionNetwork.create().show();
+            networkInfo = connectivityManager.getActiveNetworkInfo();
         }
     }
-
-    public void editSetting() {
-        Log.e("aa", "editSetting function");
-        Intent intent = new Intent(Settings.ACTION_WIFI_SETTINGS);
-        startActivity(intent);
-    }
-
 }
